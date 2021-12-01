55ed015ae3b4c83ffb80aefbca0ea9a9dbeead0e
fastocher <fastocher@yandex.ru>
2021-12-02 00:26:22 +0300

    make it better

--- src/main/java/NC/mtroom/room/impl/repository/EquipmentRepository.java
+++ src/main/java/NC/mtroom/room/impl/repository/EquipmentRepository.java
@@ -6,10 +6,9 @@ import NC.mtroom.room.impl.entity.Equipment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 
 @Repository
 public interface EquipmentRepository extends CrudRepository<Equipment, Long> {
-//    List<Equipment> findAll();
+
 }
