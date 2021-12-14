656577f401160ab105ed2a2ec5f4aa3f0ae641bc
fastocher <fastocher@yandex.ru>
2021-12-14 19:02:35 +0300

    make it better

--- src/main/java/NC/mtroom/JWTConfig/JwtTokenUtil.java
+++ src/main/java/NC/mtroom/JWTConfig/JwtTokenUtil.java
@@ -18,7 +18,7 @@ public class JwtTokenUtil implements Serializable {
 
     private static final long serialVersionUID = -2550185165626007488L;
     // задаем срок действия.  желаемое количество часов * 60 секунд * 60 минут
-    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+    public static final long JWT_TOKEN_VALIDITY = 10 * 60 * 60;
 
     @Value("${jwt.secret}")
     private String secret;
